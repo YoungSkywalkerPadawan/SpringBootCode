7899149b0975f159d9d045add841250a4aaa3ca4
Kai <2504391561@qq.com>
2024-03-28 18:01:13 +0800

    spring boot source code

---
+++ skywalker-web/src/main/java/com/example/skywalkerweb/SkywalkerWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.skywalkerweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkywalkerWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkywalkerWebApplication.class, args);
+	}
+
+}
---
+++ skywalker-web/src/test/java/com/example/skywalkerweb/SkywalkerWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.skywalkerweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkywalkerWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
